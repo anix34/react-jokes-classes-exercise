1fef126cf7ea78ba808a9aacc694a1031542b505
anix34 <139282344+anix34@users.noreply.github.com>
2024-07-04 07:42:13 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import JokeList from "./JokeList";
+
+/** App component. Renders list of jokes. */
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <JokeList />
+//       </div>
+//     );
+//   }
+// }
+
+const App = ()=>{
+  return (
+    <div className="App">
+      <JokeList />
+    </div>
+  )
+}
+
+export default App;
